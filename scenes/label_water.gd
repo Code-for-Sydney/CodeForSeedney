4389eff5a3bfbbc4170d365ca1b94b7eb6f95515
Simon Bunker <simon@rendermania.com>
2025-10-04 13:44:47 +1000

    added water button

---
+++ scenes/label_water.gd
@@ -0,0 +1,16 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.text = str(Global.water)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.text = str(Global.water)
+
+
+func _on_button_buy_water_pressed() -> void:
+	Global.water += 10
+	Global.budget -= 10
