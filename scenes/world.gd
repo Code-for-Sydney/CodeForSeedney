cc5bbccd6e0655fa10cefb0e0e88aa74a324851f
Simon Bunker <simon@rendermania.com>
2025-10-04 22:18:52 +1000

    fixed seeding offset. data dictionaries are now based off crop tile sizes rather than ground as they are different now

--- scenes/world.gd
+++ scenes/world.gd
@@ -37,7 +37,7 @@ func _input(event):
 			var data = ground.get_cell_tile_data(tile_pos)
 			if data:
 				var tile_name = data.get_custom_data("tile_name")
-				watering_tile(tile_name, tile_pos, 3)
+				watering_tile(tile_name, tile_pos, 1.0)
 				
 			harvesting(tile_pos)
 				
@@ -46,8 +46,8 @@ func _input(event):
 			crop[tile_pos] = { "name" : Global.current_tool, "duration" : 0 }
 
 func get_snapped_position(global_pos: Vector2) -> Vector2i:
-	var local_pos = ground.to_local(global_pos)
-	var tile_pos = ground.local_to_map(local_pos)
+	var local_pos = crop_layer.to_local(global_pos)
+	var tile_pos = crop_layer.local_to_map(local_pos)
 	return tile_pos
 
 func set_tile(tile_name: String, cell_pos: Vector2i, layer: TileMapLayer, coord: int = 0):
@@ -57,15 +57,15 @@ func set_tile(tile_name: String, cell_pos: Vector2i, layer: TileMapLayer, coord:
 func watering_tile(tile_name: String, pos: Vector2i, amount: float = 1.0):
 	if Global.water > 0:
 		water_level[pos] = amount
-		set_tile(tile_name, pos, ground, 1)
+		#set_tile(tile_name, pos, ground, 1)
 		Global.water -= 1
 
 func drying_tile(pos):
 	var tile_pos = get_snapped_position(pos)
 	var data = ground.get_cell_tile_data(tile_pos)
-	if data:
-		var tile_name = data.get_custom_data("tile_name")
-		set_tile(tile_name, pos, ground)
+	#if data:
+		#var tile_name = data.get_custom_data("tile_name")
+		#set_tile(tile_name, pos, ground)
 
 func harvesting(pos):
 	if crop_layer.get_cell_source_id(pos) != -1 and crop.has(pos) and crop[pos]["duration"] < 0:
